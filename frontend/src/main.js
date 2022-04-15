0cdd367a560b94976a10152190289550a48602d4
sergks <serg31ks@yandex.ru>
2022-04-15 17:34:26 +0300

    Добавил получение данныз через API.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,11 +9,17 @@ Vue.config.productionTip = false
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from "axios";
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.component('default-layout', DefaultLayout);
+
+// подключаем axios
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost/api'
+})
 
 new Vue({
   router,
